909e2bd074daf970143e68d2c595a0505438f578
MilenaSf00 <milenasoaresf2@gmail.com>
2024-08-14 12:22:19 -0300

    server.ts

---
+++ prisma/migrations/20240814150624_change_date_columns_to_date/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "projetos" ALTER COLUMN "dt_inicio_proj" SET DATA TYPE DATE,
+ALTER COLUMN "dt_fim_proj" SET DATA TYPE DATE,
+ALTER COLUMN "ult_alter_proj" SET DATA TYPE DATE;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,39 @@
-// src/server.ts
 
-import express from "express";
+// src/server.ts
+import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes/index";
+import { AppError } from "../src/errors/AppErros"; // Certifique-se de ter um arquivo AppError.ts ou ajuste o caminho conforme necessário
+
 
 const app = express();
 
 app.use(express.json()); // Middleware para analisar o corpo da requisição como JSON
 
-app.use("/api", routes); // Usa as rotas definidas no index.ts
+app.use( routes); 
+
+// Middleware para tratamento de erros
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    if (err instanceof AppError) {
+      return response.status(500).json({
+        status: "error",
+        message: `Internal server error - ${err.message}`,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Server is running on port 3333!!"));
